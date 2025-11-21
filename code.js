370f96f4b21518330eaeb47625333f7c8a34c8c0
Pabllo <pabllo164164@gmail.com>
2025-11-21 15:57:31 -0300

    feat(Added a default layout and basic Auth)

--- code.js
+++ code.js
@@ -1,15 +1,20 @@
 const WEBHOOK_URL = "";
-
-const AUTH_USER = "";
 const AUTH_PASS = "";
 
 figma.showUI(__html__, { width: 300, height: 250 });
 
+// Listen UI events
 figma.ui.onmessage = async (msg) => {
+  
   if (msg.type === "login") {
-    // No authentication anymore
-    figma.clientStorage.setAsync("session", "ok");
-    figma.notify("Logged in!");
+    if (msg.password !== AUTH_PASS) {
+      figma.notify("Senha incorreta!");
+      return;
+    }
+
+    await figma.clientStorage.setAsync("session", "ok");
+    figma.ui.postMessage({ type: "logged-in" });
+    figma.notify("Logado!");
   }
 
   if (msg.type === "send") {
@@ -18,39 +23,25 @@ figma.ui.onmessage = async (msg) => {
 };
 
 async function sendImage() {
-  console.log("➡️ sendImage() called");
-
   const selection = figma.currentPage.selection;
-  console.log("📌 Selection:", selection);
 
   if (selection.length !== 1) {
-    console.log("❌ Invalid selection length:", selection.length);
-    figma.notify("Select exactly one frame");
+    figma.notify("Selecione um frame apenas!");
     return;
   }
 
   const node = selection[0];
-  console.log("🖼️ Selected node:", node.name, node.type);
-
   const pngBytes = await node.exportAsync({ format: "PNG" });
-  console.log("📦 PNG bytes length:", pngBytes.byteLength);
-
-  console.log("🌐 Sending POST request to:", WEBHOOK_URL);
 
   const res = await fetch(WEBHOOK_URL, {
     method: "POST",
     body: new Uint8Array(pngBytes)
   });
 
-  console.log("📥 Server response status:", res.status);
-
   if (!res.ok) {
-    console.log("❌ Response not OK:", res);
-    figma.notify("Failed to send image");
+    figma.notify("Falha ao enviar a mensage, por favor tente mais tarde!");
     return;
   }
 
-  figma.notify("Image sent!");
-  console.log("✅ Image sent successfully");
+  figma.notify("Frame enviado!");
 }
-
